d607b4349b8b754baeddce6f7f242d0e1ddce426
Evilintruder <169144970+Evilintruder@users.noreply.github.com>
2026-03-14 03:59:11 +0100

    Restrict CORS to specific origins

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const sendEmailRoute = require("./routes/sendEmail");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://morganticket.site"
+  ]
+}));
 app.use(express.json());
 
 app.use("/api/send-email", sendEmailRoute);
