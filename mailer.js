077a929f4854acbb9d9f09212c8f1120142cb067
Evilintruder <169144970+Evilintruder@users.noreply.github.com>
2026-03-14 13:10:38 +0100

    Update mailer.js to use SendGrid for SMTP

--- mailer.js
+++ mailer.js
@@ -3,15 +3,13 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
 
-  host: process.env.SMTP_HOST,
-
-  port: 465,
-
-  secure: true, // IMPORTANT for port 465
+  host: "smtp.sendgrid.net",
+  port: 587,
+  secure: false,
 
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
+    user: "apikey",
+    pass: process.env.SENDGRID_API_KEY
   }
 
 });
